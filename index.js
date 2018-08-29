1973bd1e7713173a41955d5c0d950add55d10f4b
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-29 16:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, driverRevenue) {
   return drivers.filter(function (driver) {
-    driver.revenue > driverRevenue
+    driver[revenue] > driverRevenue
   })
 }
