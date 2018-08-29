e3d48b793638c39574f1c4556149feb10b8f5f66
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-29 16:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, driverRevenue) {
-  return drivers.filter(function (driver) => driver["revenue"] > driverRevenue)
+  return drivers.filter( (driver) => driver["revenue"] > driverRevenue)
 }
 
 function driverNamesWithRevenueOver(drivers, driverRevenue) {
